6345d3b4ac2804e6c7b241611f0ec50c896f05ad
gurjyan <gurjyan@jlab.org>
2023-03-17 09:30:15 -0400

    Created json file output for SAMPA raw data (decoded).

--- src/main/java/org/jlab/ersap/actor/sampa/engine/SampaFileSinkEngine.java
+++ src/main/java/org/jlab/ersap/actor/sampa/engine/SampaFileSinkEngine.java
@@ -1,15 +1,19 @@
 package org.jlab.ersap.actor.sampa.engine;
 
+import com.google.gson.Gson;
+import org.jlab.epsci.ersap.base.error.ErsapException;
 import org.jlab.epsci.ersap.engine.EngineDataType;
 import org.jlab.epsci.ersap.std.services.AbstractEventWriterService;
 import org.jlab.epsci.ersap.std.services.EventWriterException;
+import org.jlab.ersap.actor.datatypes.DasDataType;
 import org.json.JSONObject;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
@@ -29,15 +33,28 @@ public class SampaFileSinkEngine extends AbstractEventWriterService<FileOutputSt
     private static String FILE_OUTPUT = "file_output";
     private boolean file_output = false;
 
+    private static String FEC_COUNT = "fec_count";
+
+    private Map<Integer, double[]> frame = new HashMap<>();
+    private int chNum;
+
+    private Gson gson = new Gson();
+
+
     @Override
     protected FileOutputStream createWriter(Path file, JSONObject opts)
             throws EventWriterException {
+
         if (opts.has(FILE_OUTPUT)) {
             if(opts.getString(FILE_OUTPUT).equalsIgnoreCase("true")) {
                 file_output = true;
             } else {
                 file_output = false;
             }
+        }
+        if (opts.has(FEC_COUNT)) {
+            // Each FEC has 2 GBT stream, each having 80 channel data
+            chNum = 80 * opts.getInt(FEC_COUNT) * 2;
         }
 
         try {
@@ -64,8 +81,31 @@ public class SampaFileSinkEngine extends AbstractEventWriterService<FileOutputSt
             try {
                 evt_count++;
                 ByteBuffer b = (ByteBuffer) event;
-                writer.write(b.array());
-                if (evt_count >= 100) {
+                ByteBuffer[] data = null;
+
+                try {
+                    data = DasDataType.deserialize(b);
+                } catch (ErsapException e) {
+                    e.printStackTrace();
+                }
+                // How much data do we have?
+                assert data != null;
+                int sampleLimit = data[0].limit()/2;
+
+                double [] dataPts = new double[sampleLimit];
+
+                for (int channel = 0; channel < chNum; channel++) {
+                    for (int sample = 0; sample < sampleLimit; sample++) {
+                        try {
+                            dataPts[sample] = data[channel].getShort(2 * sample); // ADC sample
+                        } catch (IndexOutOfBoundsException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    frame.put(channel,dataPts);
+                }
+                writer.write(gson.toJson(frame).getBytes());
+                 if (evt_count >= 1000) {
                     writer.flush();
                     writer.close();
                     f_count++;
