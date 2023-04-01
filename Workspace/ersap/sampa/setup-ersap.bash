dce6e7e692864c50f307b8c9ffed11a1009b37bc
gurjyan <gurjyan@jlab.org>
2023-04-01 10:18:35 -0400

    deleted Workspace diectory

--- Workspace/ersap/sampa/setup-ersap.bash
+++
@@ -1,8 +0,0 @@
-
-unset CLASSPATH
-export JAVA_HOME=/daqfs/java/jdk-11.0.2
-
-export ERSAP_HOME=`pwd`/myErsap
-export ERSAP_USER_DATA=`pwd`/user_data
-
-export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler"
--- Workspace/ersap/sampa/setup-ersap.tcsh
+++
@@ -1,8 +0,0 @@
-
-unset CLASSPATH
-setenv JAVA_HOME /daqfs/java/jdk-11.0.2
-
-setenv ERSAP_HOME `pwd`/myErsap
-setenv ERSAP_USER_DATA `pwd`/user_data
-
-setenv JAVA_TOOL_OPTIONS "-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler"
