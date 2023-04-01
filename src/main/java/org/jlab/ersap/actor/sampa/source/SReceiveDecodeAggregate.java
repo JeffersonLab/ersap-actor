5bbd2f60516c0612cfd3262d415a07ab1cd7de40
gurjyan <gurjyan@jlab.org>
2023-04-01 09:20:36 -0400

    increase of the ring buffers. Testing to see the effect of dropped sample frames.

--- src/main/java/org/jlab/ersap/actor/sampa/source/SReceiveDecodeAggregate.java
+++ src/main/java/org/jlab/ersap/actor/sampa/source/SReceiveDecodeAggregate.java
@@ -33,7 +33,7 @@ public class SReceiveDecodeAggregate extends Thread {
     private int activeStreams;
 
     // Max ring items
-    private final static int maxRingItems = 1048576;
+    private final static int maxRingItems = 80192;
 
     // Aggregated RingBuffer for data transfer between aggregator and this object.
     private final RingBuffer<SRingRawEvent> aggRingBuffer;
