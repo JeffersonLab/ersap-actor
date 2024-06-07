035c37fc0ea98dcac202d2ab675b04171e2f772c
gurjyan <gurjyan@jlab.org>
2024-06-06 23:10:49 -0400

    Created an empty engine

---
+++ src/main/java/org/jlab/ersap/actor/coda/engine/EmptyEngine.java
@@ -0,0 +1,64 @@
+package org.jlab.ersap.actor.coda.engine;
+
+import org.jlab.epsci.ersap.engine.Engine;
+import org.jlab.epsci.ersap.engine.EngineData;
+import org.jlab.epsci.ersap.engine.EngineDataType;
+
+import java.util.Set;
+
+public class EmptyEngine implements Engine {
+    @Override
+    public EngineData configure(EngineData engineData) {
+        return null;
+    }
+
+    @Override
+    public EngineData execute(EngineData engineData) {
+        return null;
+    }
+
+    @Override
+    public EngineData executeGroup(Set<EngineData> set) {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getInputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getOutputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
