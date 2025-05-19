ca736cc71dfeff0ebbc57bc41ea9f3373ca6ac9f
gurjyan <gurjyan@jlab.org>
2025-05-18 22:31:11 -0400

    oops. I mean ByteBuffer

--- src/main/java/org/jlab/ersap/actor/coda/source/socket/Event.java
+++ src/main/java/org/jlab/ersap/actor/coda/source/socket/Event.java
@@ -1,13 +1,16 @@
 package org.jlab.ersap.actor.coda.source.socket;
 
+import java.nio.ByteBuffer;
+
 /**
  * Event class for use with LMAX Disruptor RingBuffer.
  */
 public class Event {
     private byte[] data;
 
-    public byte[] getData() {
-        return data;
+    public ByteBuffer getData() {
+
+        return ByteBuffer.wrap(data);
     }
 
     public void setData(byte[] data) {
