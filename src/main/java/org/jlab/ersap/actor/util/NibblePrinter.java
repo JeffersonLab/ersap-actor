6600c7e551b5452553943db58e2a6261b326ea1d
gurjyan <gurjyan@jlab.org>
2025-06-13 10:23:22 -0400

    NibblePrinter class to try to understand the EVIO files we have.

--- src/main/java/org/jlab/ersap/actor/util/NibblePrinter.java
+++ src/main/java/org/jlab/ersap/actor/util/NibblePrinter.java
@@ -1,23 +1,43 @@
 package org.jlab.ersap.actor.util;
 
 public class NibblePrinter {
-    public static void printNibbles(byte[] data) {
+    public static void printNibbles(byte[] data, int targetCount) {
         int nibbleCount = 0;
+        int matchCount = 0;
 
-        for (byte b : data) {
-            int highNibble = (b >> 4) & 0x0F;
-            int lowNibble = b & 0x0F;
+        for (int i = 0; i < data.length - 3; i++) {
+            int word = ((data[i] & 0xFF) << 24) | ((data[i + 1] & 0xFF) << 16)
+                    | ((data[i + 2] & 0xFF) << 8) | (data[i + 3] & 0xFF);
 
-            System.out.print(Integer.toHexString(highNibble));
-            nibbleCount++;
-            if (nibbleCount % 8 == 0) System.out.println();
+            if (word == 0xc0da0100) {
+                matchCount++;
+            }
 
-            System.out.print(Integer.toHexString(lowNibble));
-            nibbleCount++;
-            if (nibbleCount % 8 == 0) System.out.println();
+            if (matchCount == targetCount) {
+                break;
+            }
+
+            // Print 4 bytes (8 nibbles)
+            for (int j = 0; j < 4 && i + j < data.length; j++) {
+                byte b = data[i + j];
+                int highNibble = (b >> 4) & 0x0F;
+                int lowNibble = b & 0x0F;
+
+                System.out.print(Integer.toHexString(highNibble));
+                nibbleCount++;
+                if (nibbleCount % 8 == 0) System.out.println();
+
+                System.out.print(Integer.toHexString(lowNibble));
+                nibbleCount++;
+                if (nibbleCount % 8 == 0) System.out.println();
+            }
+
+            if (word == 0xc0da0100) {
+                System.out.println("\n--- CODA MAGIC WORD ---\n");
+            }
         }
 
-        // If the last line wasn't terminated, add a newline.
         if (nibbleCount % 8 != 0) System.out.println();
     }
+
 }
