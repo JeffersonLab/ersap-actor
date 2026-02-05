d7c0ea366cceb6828fc17ae6a06ac4ab0fb546bf
gurjyan <gurjyan@jlab.org>
2026-02-05 11:30:43 -0500

    Sink engine does not write on console

--- src/main/java/org/jlab/ersap/actor/coda/engine/CodaSinkEngine.java
+++ src/main/java/org/jlab/ersap/actor/coda/engine/CodaSinkEngine.java
@@ -31,7 +31,6 @@ public class CodaSinkEngine extends AbstractEventWriterService<FileWriter> {
 
     @Override
     protected void writeEvent(Object o) throws EventWriterException {
-        System.out.println(o);
     }
 
     @Override
